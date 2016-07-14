34189b13faa6330c22d6910df6527df2eee4a049
Linda Zou <linda.ye.zou@gmail.com>
2016-07-13 19:58:03 -0700

    Sets up initial basic page layout
    - Renders basic data on html page
    - Changes JSON data format
    
    	modified:   apartment.json
    	modified:   controllers/AptListController.js
    	modified:   index.html
    	modified:   instruction.pdf
    	modified:   styles/style.css
    	modified:   views/apt-list.html

--- controllers/AptListController.js
+++ controllers/AptListController.js
@@ -1,6 +1,10 @@
 angular.module("aptApp.AptListController", ['ui.bootstrap'])
 
 .controller("AptListController", function($scope, $http, $location) {
-
+	$http.get('apartment.json')
+	  .then(function(res){
+	    $scope.data = res.data;  
+	    console.log($scope.data);
+	  });
 });
 
