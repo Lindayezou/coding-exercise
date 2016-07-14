8b3b10282ade479f73e83d1eb27913f3f7989945
Linda Zou <linda.ye.zou@gmail.com>
2016-07-13 23:22:39 -0700

    Implements pagination
    	modified:   apartment.json
    	modified:   bower.json
    	modified:   controllers/AptDetailController.js
    	modified:   controllers/AptListController.js
    	modified:   index.html
    	modified:   views/apt-detail.html
    	modified:   views/apt-list.html

--- controllers/AptDetailController.js
+++ controllers/AptDetailController.js
@@ -1,6 +1,10 @@
 angular.module("aptApp.AptDetailController", ['ui.bootstrap'])
 
 .controller("AptDetailController", function($scope, $http, $location, $window) {
-$scope.apt = $window.localStorage.getItem('aptName')
+	$scope.apt = $window.localStorage.getItem('aptName')
+
+	$scope.listPage = function() {
+		$location.path('/apt-list');
+	} 
 
 });
--- controllers/AptListController.js
+++ controllers/AptListController.js
@@ -1,18 +1,23 @@
-angular.module("aptApp.AptListController", ['ui.bootstrap'])
+angular.module("aptApp.AptListController", ['ui.bootstrap', 'angularUtils.directives.dirPagination'])
 
 .controller("AptListController", function($scope, $http, $location, $window) {
 	$scope.buttons = ["popularity", "price"]
 	$scope.sort = "popularity";
+	$scope.data = [];
+	$scope.pageData = [];
+	$scope.maxSize = 5;
+	$scope.currentPage = 1;
+	$scope.numPerPage = 5;
+	$scope.numData;
 
 	$http.get('apartment.json')
 	  .then(function(res){
 	    $scope.data = res.data;  
-	    console.log($scope.data);
 	  });
 
 	$scope.detailsPage = function(apt) {
 		console.log("I want this one: ", apt)
-		$window.localStorage.setItem('aptName', apt.name );
+		$window.localStorage.setItem('aptName', apt.name);
 		$location.path('/apt-detail');
 	}  
 
